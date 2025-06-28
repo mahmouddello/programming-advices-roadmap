0f901f599ce03630b4f0c3924faa54792992a96f
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-06-29 02:35:10 +0300

    01 - First C# Project

---
+++ 14 - C# Level 1/01 - First C# Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01___First_C__Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is my first C# Project :-)");
+            Console.ReadLine(); // leaves the console window open, similiar to system("pause > 0") in C++
+        }
+    }
+}
