51a60acaeb211b2a92fa16aadc05c49e504c9563
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-07-02 14:39:17 +0300

    Add Syntax, Output, Comments Lessons

---
+++ 14 - C# Level 1/02 - Syntax/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // instead of #include we use "using" in C#
+
+namespace Syntax // Namespace: we can name it what ever we want, same as C++
+{
+    internal class Program
+    {
+        // main can't be written outside of class like C++, C# forces OOP
+        // main is Static, so we can call it without an Object
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World :)"); // writes line to the screen
+            Console.ReadKey(); // holds the console window
+        }
+    }
+}
---
+++ 14 - C# Level 1/03 - Output/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // WriteLine() = cout in C++, automatically inserts new line after end of statement
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My Name is Mahmoud Dello");
+            Console.WriteLine("I'm learning C#");
+            Console.WriteLine("C# is much easier than C++ to learn :)");
+            Console.WriteLine("Sum of 10 + 20 = " + (10 + 20)); // without parantheses, it will peform string concatenation.
+
+            // Write() = like cout with out endl or '\n' 
+            Console.Write("I'm a Junior Software Engineer");
+            Console.Write(", Passionate in Back-End Development");
+
+            // Formatted String
+
+            // 0 -> first paramter : Welcome to, 1 -> Second Parameter: Programming Advices
+            Console.WriteLine("\n{0} {1}", "Welcome to", "Programming Advices");
+            Console.WriteLine("Hi MyName is: {0} I live in {1}", "Mahmoud", "Turkey");
+
+            // Escape Characters
+            Console.WriteLine("Useful Escape Characters:\n");
+
+            //Newline
+            Console.WriteLine("Newline:");
+            Console.WriteLine("Welcome to \nProgrammingAvices\n");
+
+            //Tab
+            Console.WriteLine("Tab:");
+            Console.WriteLine("Welcome to\tProgrammingAvices\n");
+
+            //Backspace
+            Console.WriteLine("Backspace:");
+            Console.WriteLine("Welcome to \bProgrammingAvices\n");
+
+            //Single quote
+            Console.WriteLine("Single Quote:");
+            Console.WriteLine("Welcome to \'ProgrammingAvices\n");
+
+            //Double quote
+            Console.WriteLine("Double Quote:");
+            Console.WriteLine("Welcome to \"ProgrammingAvices\n");
+
+            //Backslash
+            Console.WriteLine("Backslash:");
+            Console.WriteLine("Welcome to \\ProgrammingAvices\n");
+
+            //Alert
+            Console.WriteLine("Alert:");
+            Console.WriteLine("\a");
+
+            Console.ReadKey();
+
+            // Single Line Comment
+            /*
+                Multiple Line Comment
+             */
+        }
+    }
+}
