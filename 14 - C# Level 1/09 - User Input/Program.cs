1d9bfbad8dde9645f929b7ddb2d27e2fae5a2afb
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-07-06 17:49:49 +0300

    09 - User Input

---
+++ 14 - C# Level 1/09 - User Input/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.ReadLine() = cin>> in C++
+            // Important: Console.ReadLine() always reads string.
+
+            // Type your username and press enter
+            Console.WriteLine("Enter username?");
+
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Username is: {userName}");
+
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine()); // convert type after reading
+            Console.WriteLine($"Number is {number}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
