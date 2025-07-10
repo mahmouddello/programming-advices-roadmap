62490fe4b0f4f7045a825c5ceba95b6b36f97e8a
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-07-10 23:26:54 +0300

    15 - Excepetions and Misc.

---
+++ 14 - C# Level 1/15 - Exceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _15___Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Random rnd = new Random();
+
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine(rnd.Next(10, 20)); // returns random integers >= 10 and < 20
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
