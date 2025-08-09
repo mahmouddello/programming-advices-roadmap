2a69af39c946ccb9ad7cd8c87a561e9510cbb99a
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-08-09 10:33:05 +0300

    TreeView and ImageList

---
+++ 14 - C# Level 1/22 - TreeView and ImageList/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace _22___TreeView_and_ImageList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Ali");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Ahmad");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Boys", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Aliaa");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Tala");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Girls", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode5});
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "2020-10-10_10-32-20-69d03d986e3001cfb40c457eede13a58.jpg");
+            this.imageList1.Images.SetKeyName(1, "242390692-0b335028-1d3d-4ee5-b5b3-a373d499be7e.gif");
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(107, 43);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Ali";
+            treeNode1.Text = "Ali";
+            treeNode2.Name = "Node3";
+            treeNode2.Text = "Ahmad";
+            treeNode3.Name = "boys";
+            treeNode3.Text = "Boys";
+            treeNode4.ImageIndex = 1;
+            treeNode4.Name = "Node5";
+            treeNode4.Text = "Aliaa";
+            treeNode5.ImageIndex = 1;
+            treeNode5.Name = "Node7";
+            treeNode5.Text = "Tala";
+            treeNode6.ImageIndex = 1;
+            treeNode6.Name = "Node4";
+            treeNode6.Text = "Girls";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode6});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(592, 347);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            this.treeView1.AfterCollapse += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCollapse);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "frmTreeView";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ 14 - C# Level 1/22 - TreeView and ImageList/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _22___TreeView_and_ImageList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            this.CheckTreeViewNode(e.Node);
+        }
+
+        private void CheckTreeViewNode(TreeNode node)
+        {
+            foreach (TreeNode childNode in node.Nodes)
+            {
+                childNode.Checked = node.Checked;
+
+                if (childNode.Nodes.Count > 0)
+                    this.CheckTreeViewNode(childNode);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Add("Hamdi");
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(treeView1.SelectedNode.Text);
+        }
+
+        private void treeView1_AfterCollapse(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            this.CheckTreeViewNode(e.Node);
+
+        }
+    }
+}
