4ce7fe7c19574a716f1cf81dea23486bf8006860
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-08-13 13:20:48 +0300

    ErrorProvider Control

---
+++ 14 - C# Level 1/24 - ErrorProvider Control/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace _24___ErrorProvider_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbMiddleName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.errPrvFormValidation = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errPrvFormValidation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Middle Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Last Name";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFirstName.Location = new System.Drawing.Point(230, 117);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(322, 45);
+            this.tbFirstName.TabIndex = 3;
+            this.tbFirstName.Validating += new System.ComponentModel.CancelEventHandler(this.tbFirstName_Validating);
+            // 
+            // tbMiddleName
+            // 
+            this.tbMiddleName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMiddleName.Location = new System.Drawing.Point(230, 181);
+            this.tbMiddleName.Name = "tbMiddleName";
+            this.tbMiddleName.Size = new System.Drawing.Size(322, 45);
+            this.tbMiddleName.TabIndex = 4;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLastName.Location = new System.Drawing.Point(230, 244);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(322, 45);
+            this.tbLastName.TabIndex = 5;
+            this.tbLastName.Validating += new System.ComponentModel.CancelEventHandler(this.tbLastName_Validating);
+            // 
+            // errPrvFormValidation
+            // 
+            this.errPrvFormValidation.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbMiddleName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.errPrvFormValidation)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbMiddleName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.ErrorProvider errPrvFormValidation;
+    }
+}
+
---
+++ 14 - C# Level 1/24 - ErrorProvider Control/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace _24___ErrorProvider_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbFirstName_Validating(object sender, CancelEventArgs e)
+        {
+            string ProcessedText = tbFirstName.Text.Trim();  
+
+            if (string.IsNullOrEmpty(ProcessedText))
+            {
+                e.Cancel = true; // prevents user from leaving textbox when there is an error
+                tbFirstName.Focus();
+                errPrvFormValidation.SetError(tbFirstName, "This field can't be empty!");
+            }
+            else
+            {
+                e.Cancel = false;
+                errPrvFormValidation.SetError(tbFirstName, ""); // removing the error
+            }
+        }
+
+        private void tbLastName_Validating(object sender, CancelEventArgs e)
+        {
+            string ProcessedText = tbLastName.Text.Trim();
+
+            if (string.IsNullOrEmpty(ProcessedText))
+            {
+                e.Cancel = true; // prevents user from leaving textbox when there is an error
+                tbLastName.Focus();
+                errPrvFormValidation.SetError(tbLastName, "This field can't be empty!");
+            }
+            else
+            {
+                e.Cancel = false;
+                errPrvFormValidation.SetError(tbLastName, ""); // removing the error
+            }
+        }
+
+        // Allows user to close the application even if there's an error triggered
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = false;
+        }
+    }
+}
