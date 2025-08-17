bec63d8b0132ba078437c7c55244863eb740ff4e
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-08-17 20:02:31 +0300

    MDI Container

---
+++ 14 - C# Level 1/26 - MDI Container/ChildForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _26___MDI_Container
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 14 - C# Level 1/26 - MDI Container/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _26___MDI_Container
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private ChildForm form = new ChildForm();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (form.IsDisposed)
+                form = new ChildForm();
+
+            form.MdiParent = this;
+            form.Show();
+        }
+    }
+}
