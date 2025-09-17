d8a4dc09069530da86ff85cfbc35101bf587e79e
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-09-18 01:38:08 +0300

    Simple OOP Calculator

---
+++ 16 - OOP As it Should be in C#/04 - Calculator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04___Calculator
+{
+    class clsCalculator
+    {
+        private float _result = 0;
+        private float _lastNumber = 0;
+        private string _lastOpeartion = string.Empty;
+
+        public void Add(float number)
+        {
+            _lastNumber = number;
+            _result += _lastNumber;
+            _lastOpeartion = "Adding";
+        }
+
+        public void Substract(float number)
+        {
+            _lastNumber = number;
+            _result -= _lastNumber;
+            _lastOpeartion = "Substracing";
+        }
+
+        public void Multiply(float number)
+        {
+            _lastNumber = number;
+            _result *= _lastNumber;
+            _lastOpeartion = "Multiplying";
+        }
+
+        public void Divide(float number)
+        {
+            if (number == 0)
+                number = 1;
+
+            _lastNumber = number;
+            _result /= _lastNumber;
+            _lastOpeartion = "Dividing";
+        }
+
+        public void Clear()
+        {
+            _result = 0;
+            _lastOpeartion = "Clear";
+        }
+
+        public void PrintResult()
+        {
+            Console.WriteLine($"Result after {_lastOpeartion} {_lastNumber} is: {_result}");
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsCalculator calculator = new clsCalculator();
+
+            calculator.Clear();
+
+            calculator.Add(10);
+            calculator.PrintResult();
+
+            calculator.Add(100);
+            calculator.PrintResult();
+
+            calculator.Substract(20);
+            calculator.PrintResult();
+
+            calculator.Divide(0);
+            calculator.PrintResult();
+
+            calculator.Divide(2);
+            calculator.PrintResult();
+
+            calculator.Multiply(3);
+            calculator.PrintResult();
+
+            Console.ReadKey();
+        }
+    }
+}
