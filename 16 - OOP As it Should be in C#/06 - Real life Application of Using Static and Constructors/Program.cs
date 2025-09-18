ca72cde2979786a566f477eab37eb6f0f5afff66
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-09-18 13:14:36 +0300

    Real life application of using * Static Methods * Constructor

---
+++ 16 - OOP As it Should be in C#/06 - Real life Application of Using Static and Constructors/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealLifeApplication
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(int Id, string Name, int Age)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public static Person Find(int Id)
+        {
+            // We simulate the process for now, later on we will use database
+            if (Id == 10)
+                return new Person(10, "Mahmoud Dello", 22);
+
+            return null;
+        }
+
+        public static Person Find(string Username, string Password)
+        {
+            // We simulate the process for now, later on we will use database
+            if (Username == "user1" && Password == "1234")
+                return new Person(11, "Hamdi Alkadi", 22);
+
+            return null;
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Person person1 = new Person(); -> Object with no params not allowed!
+            Person person2 = new Person(11, "Jawad Dello", 14);
+
+            Console.WriteLine("Finding Person by ID");
+            
+            Person person3 = Person.Find(Id: 10);
+            if (person3 != null)
+            {
+                Console.WriteLine($"ID = {person3.Id}");
+                Console.WriteLine($"Name = {person3.Name}");
+                Console.WriteLine($"Age = {person3.Age}");
+            }
+            else
+                Console.WriteLine("Couldn't find the specfied person by given Id.");
+
+            Console.WriteLine("\nFinding Person by username and password");
+
+            Person person4 = Person.Find(Username: "user1", Password: "1234");
+            if (person3 != null)
+            {
+                Console.WriteLine($"ID = {person4.Id}");
+                Console.WriteLine($"Name = {person4.Name}");
+                Console.WriteLine($"Age = {person4.Age}");
+            }
+            else
+                Console.WriteLine("Couldn't find the specfied person by given username and password.");
+        }
+    }
+}
+
---
+++ 16 - OOP As it Should be in C#/06 - Real life Application of Using Static and Constructors/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("06 - Real life Application of Using Static and Constructors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("06 - Real life Application of Using Static and Constructors")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a60b31f2-5c00-431e-9edc-623adb6b951b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
