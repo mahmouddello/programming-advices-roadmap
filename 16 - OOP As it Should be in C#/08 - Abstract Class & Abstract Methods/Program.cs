6e56046efa021251a9c02c0076462ae9f5f798ed
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-09-19 14:12:18 +0300

    Abstract class and Abstract Methods

---
+++ 16 - OOP As it Should be in C#/08 - Abstract Class & Abstract Methods/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08___Abstract_Class___Abstract_Methods
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void Introduce();
+
+        public void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+
+    public class Employee : Person
+    {
+        public int Id   { get; set; }
+        
+        // forced to implement abstract method by overriding it
+        public override void Introduce()
+        {
+            Console.WriteLine($"Hi, my name is {FirstName} {LastName}, and my employee id is {Id}");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            emp.FirstName = "Mahmoud";
+            emp.LastName = "Dello";
+            emp.Id = 123;
+
+            emp.Introduce();
+            emp.SayGoodbye();
+
+        }
+    }
+}
