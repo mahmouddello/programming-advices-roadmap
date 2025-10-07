4c0e1adba45eeb34e4f2a64149eedd08c5a55ba7
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-10-07 23:01:49 +0300

    GUID Example in C#

---
+++ 17 - Database - SQL (Projects & Practice)/06 - GUID Example in C#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06___GUID_Example_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) 
+            {
+                Guid guid = Guid.NewGuid();
+                Console.WriteLine(guid);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
