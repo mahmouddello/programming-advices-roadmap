93bc0ba8e97a690a684ad9043324ad7318ef2522
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-11-02 19:35:32 +0300

    Add SQL problems 07 - 10

--- 17 - Database - SQL (Projects & Practice)/08 - SQL Problems/002_Problems_01_10.sql
+++ 17 - Database - SQL (Projects & Practice)/08 - SQL Problems/002_Problems_01_10.sql
@@ -1,6 +1,6 @@
 USE VehicleMakesDB;
 
--- Problem #1 - Create Master View
+-- Problem #1: Create Master View
 GO  -- separates batches
 
 CREATE VIEW VehicleMasterDetails
@@ -18,29 +18,29 @@ FROM VehicleDetails vd
 INNER JOIN Makes m ON vd.MakeID = m.MakeID
 INNER JOIN MakeModels mm ON vd.ModelID = mm.ModelID
 INNER JOIN SubModels sm ON vd.SubModelID = sm.SubModelID
-INNER JOIN Bodies b ON vd.BodyID = b.BodyID
-INNER JOIN DriveTypes dt ON vd.DriveTypeID = dt.DriveTypeID
-INNER JOIN FuelTypes ft ON vd.FuelTypeID = ft.FuelTypeID;
+LEFT JOIN Bodies b ON vd.BodyID = b.BodyID
+LEFT JOIN DriveTypes dt ON vd.DriveTypeID = dt.DriveTypeID
+LEFT JOIN FuelTypes ft ON vd.FuelTypeID = ft.FuelTypeID;
 GO  -- separate again
 
 SELECT * FROM VehicleMasterDetails;
 
--- Problem #2 - Get All Vehicle Between 1950 and 2000
+-- Problem #2: Get All Vehicle Between 1950 and 2000
 SELECT * FROM VehicleMasterDetails
 WHERE [Year] BETWEEN 1950 AND 2000;
 
--- Problem #3 - Get number vehicles made between 1950 and 2000
+-- Problem #3: Get number vehicles made between 1950 and 2000
 SELECT COUNT(*) AS NumberOfVehicles FROM VehicleDetails
 WHERE [Year] BETWEEN 1950 AND 2000;
 
--- Problem #4 - Get number vehicles made between 1950 and 2000 per make and order them by Number Of Vehicles Descending
+-- Problem #4: Get number vehicles made between 1950 and 2000 per make and order them by Number Of Vehicles Descending
 SELECT m.Make, COUNT(*) AS NumberOfVehicles FROM VehicleDetails vd
 INNER JOIN Makes m ON vd.MakeID = m.MakeID
 WHERE [Year] BETWEEN 1950 AND 2000
 GROUP BY m.Make
 ORDER BY NumberOfVehicles DESC;
 
--- Problem #5 - Get All Makes that have manufactured more than 12000 Vehicles in years 1950 to 2000
+-- Problem #5: Get All Makes that have manufactured more than 12000 Vehicles in years 1950 to 2000
 SELECT m.Make, COUNT(*) AS NumberOfVehicles FROM VehicleDetails vd
 INNER JOIN Makes m ON vd.MakeID = m.MakeID
 WHERE ([Year] BETWEEN 1950 AND 2000)
@@ -58,7 +58,7 @@ SELECT * FROM (
 WHERE R1.NumberOfVehicles > 12000
 ORDER BY R1.NumberOfVehicles DESC;
 
--- Problem #6 - Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside
+-- Problem #6: Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside
 SELECT * FROM (
     SELECT m.Make, COUNT(*) AS NumberOfVehicles, (SELECT COUNT(*) FROM VehicleDetails) AS TotalVehicles FROM VehicleDetails vd
     INNER JOIN Makes m ON vd.MakeID = m.MakeID
@@ -68,12 +68,42 @@ SELECT * FROM (
 ORDER BY R2.NumberOfVehicles DESC;
 -- Lesson learned from this problem, we can treat entire query as a column
 
--- Problem #7 - Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it,
--- then calculate it's percentage
-SELECT * FROM (
-    SELECT m.Make, COUNT(*) AS NumberOfVehicles, (SELECT COUNT(*) FROM VehicleDetails) AS TotalVehicles FROM VehicleDetails vd
-    INNER JOIN Makes m ON vd.MakeID = m.MakeID
-    WHERE [Year] BETWEEN 1950 AND 2000
-    GROUP BY m.Make
-) R2 
+-- Problem #7: Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it (Problem #6), then calculate it's percentage
+SELECT  R2.*, R2.NumberOfVehicles * 1.0 / R2.TotalVehicles AS [Percentage] FROM(
+        SELECT m.Make, COUNT(*) AS NumberOfVehicles, (SELECT COUNT(*) FROM VehicleDetails) AS TotalVehicles FROM VehicleDetails vd
+        INNER JOIN Makes m ON vd.MakeID = m.MakeID
+        WHERE [Year] BETWEEN 1950 AND 2000
+        GROUP BY m.Make
+    ) R2 
 ORDER BY R2.NumberOfVehicles DESC;
+
+-- Programming Advices
+SELECT *, CAST(NumberOfVehicles AS FLOAT) / CAST (TotalVehicles AS FLOAT) AS Perc FROM(
+        SELECT m.Make, COUNT(*) AS NumberOfVehicles, (SELECT COUNT(*) FROM VehicleDetails) AS TotalVehicles FROM VehicleDetails vd
+        INNER JOIN Makes m ON vd.MakeID = m.MakeID
+        WHERE [Year] BETWEEN 1950 AND 2000
+        GROUP BY m.Make
+    ) R2 
+ORDER BY R2.NumberOfVehicles DESC;
+
+-- Problem #8: Get Make, FuelTypeName and Number of Vehicles per FuelType per Make 
+SELECT m.Make, ft.FuelTypeName, COUNT(*) AS NumOfVehiles FROM VehicleDetails vd
+INNER JOIN Makes m ON vd.MakeID = m.MakeID
+INNER JOIN FuelTypes ft ON vd.FuelTypeID = ft.FuelTypeID
+WHERE [Year] BETWEEN 1950 AND 2000
+GROUP BY m.Make, ft.FuelTypeName
+ORDER BY m.Make;
+
+-- Problem #9: Get all vehicles that runs with GAS
+SELECT vd.*, ft.FuelTypeName FROM VehicleDetails vd
+INNER JOIN FuelTypes ft ON vd.FuelTypeID = ft.FuelTypeID
+WHERE FuelTypeName = N'GAS';
+
+SELECT * FROM VehicleMasterDetails
+WHERE FuelTypeName = N'GAS'; -- N is safe to use when searching in unicode fields
+
+-- Problem #10: Get all Makes that runs with GAS
+SELECT DISTINCT m.Make, ft.FuelTypeName FROM VehicleDetails vd
+INNER JOIN Makes m ON vd.MakeID = m.MakeID
+INNER JOIN FuelTypes ft ON vd.FuelTypeID = ft.FuelTypeID 
+WHERE ft.FuelTypeName = N'GAS';
