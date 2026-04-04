53f4af3c86e3839d94947f09cf37b7afa18e7a73
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2026-04-04 14:26:29 +0300

    02 - Delegares (Outside of roadmap)

---
+++ 20 - C# Programming Level 2/02 - Delegates/TranslatorDelegate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _02___Events
+{
+    internal class TranslatorDelegate
+    {
+        delegate string Translator(string input);
+        delegate string ChineseTranslator(string input, string accent);
+        static void Main(string[] args)
+        {
+            Translator translator = new Translator(EnglishToFrench); // method name as argument: pointing to the method
+            string result = translator.Invoke("Hi");
+            // or translator("Hi") and it will work
+            Console.WriteLine($"English --> French: {result}");
+
+            // let's test the delegate with EnglishToChinese
+            //translator = new ChineseTranslator(EnglishToChinese); // not allowed, signatre doesn't match
+
+            ChineseTranslator chineseTranslator = new ChineseTranslator(EnglishToChinese);
+            result = chineseTranslator.Invoke("Hey", "Mandarin");
+            Console.WriteLine($"English --> Chinese (Mandarin): {result}");
+        }
+
+        static string EnglishToFrench(string message)
+        {
+            return "Bonjour!";
+        }
+
+        static string EnglishToSpanish(string message)
+        {
+            return "Hola!";
+        }
+
+        static string EnglishToGerman(string message)
+        {
+            return "Hallo!";
+        }
+
+        static string EnglishToChinese(string message, string accent)
+        {
+            return "nǐ hǎo!";
+        }
+    }
+}
---
+++ 20 - C# Programming Level 2/02 - Delegates/ValuesGreaterThanDelegate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02___Events
+{
+    internal class ValuesGreaterThanDelegate
+    {
+        delegate bool ComparisionDel(int x, int y);
+
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>() { 1, 2, 4, 6, 10, 12, 15, 19 };
+
+            var output = GetFilteredValues(list, 10, IsGreaterThan); // method name as third param
+
+            Console.WriteLine("Values greater than 10:");
+            foreach (var i in output)
+                Console.Write($"{i} ");
+
+            var output2 = GetFilteredValues(list, 10, IsLessThan);
+            Console.WriteLine("\n\nValues less than 10:");
+            foreach (var i in output2)
+                Console.Write($"{i} ");
+
+            Console.WriteLine();
+        }
+
+        static List<int> GetFilteredValues(List<int> list, int m, ComparisionDel operation)
+        {
+            List<int> result = new List<int>();
+
+            foreach (int item in list)
+            {
+                if (operation(item, m))
+                    result.Add(item);
+            }
+
+            return result;
+        }
+
+        private static bool IsLessThan(int x, int y)
+        {
+            return x < y;
+        }
+
+        private static bool IsGreaterThan(int x, int y)
+        {
+            return x > y;
+        }
+
+    }
+}
