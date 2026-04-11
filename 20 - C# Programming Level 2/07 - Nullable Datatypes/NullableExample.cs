63e08b03291c5b20cfa39d7532aa11a725d638d4
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2026-04-11 16:42:08 +0300

    07 - Nullable Datatypes

---
+++ 20 - C# Programming Level 2/07 - Nullable Datatypes/NullableExample.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _07___Nullable_Datatypes
+{
+    internal class NullableExample
+    {
+        
+
+        static void Main()
+        {
+            // Declare a nullable int using Nullable<T>
+            Nullable<int> nullableInt1 = null;
+
+            // Shorthand notation using int?
+            int? nullableInt2 = null;
+
+            // Check if the nullable ints have values
+            if (nullableInt1.HasValue)
+            {
+                Console.WriteLine("nullableInt1 has a value: " + nullableInt1.Value);
+            }
+            else
+            {
+                Console.WriteLine("nullableInt1 is null.");
+            }
+
+
+            if (nullableInt2.HasValue)
+            {
+                Console.WriteLine("nullableInt2 has a value: " + nullableInt2.Value);
+            }
+            else
+            {
+                Console.WriteLine("nullableInt2 is null.");
+            }
+
+
+            // Using the null-coalescing operator
+            int result = nullableInt2 ?? 0;
+            Console.WriteLine("Using null-coalescing operator: " + result);
+
+
+            // Using the null-conditional operator
+            string stringValue = nullableInt2?.ToString();
+            Console.WriteLine("String representation: " + (stringValue ?? "null"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 20 - C# Programming Level 2/07 - Nullable Datatypes/NullableExample2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _07___Nullable_Datatypes
+{
+    internal class NullableExample2
+    {
+        static void Proedure1(string name, int? age)
+        {
+            Console.WriteLine(name);
+
+            if (age.HasValue)
+                Console.WriteLine(age);
+            else
+                Console.WriteLine("Age is null");
+        }
+
+        static void Proedure2(string name, Nullable<int> age)
+        {
+            Console.WriteLine(name);
+
+            if (age.HasValue)
+                Console.WriteLine(age);
+            else
+                Console.WriteLine("Age is null");
+        }
+
+        static void Main(string[] args)
+        {
+            Proedure1("Mahmoud", 35);
+            Proedure1("Anas", null);
+
+            Proedure2("Mahmoud", null);
+            Proedure2("Anas", 24);
+        }
+    }
+}
